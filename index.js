cb498b7a6b5e30ab34bcd5aa764d26a138a0d72a
Amanda Maree Jones <amandamareejones@gmail.com>
2024-09-14 15:04:46 +1000

    Initial setup of HTML, CSS and JS files

---
+++ index.js
@@ -0,0 +1,5 @@
+let apiKey = "09te212a1c952dfb01916b44eoad6c23";
+let context =
+  "you are an intelligent AI Assistant, who provides funny quotes about cheese";
+let prompt = "tell me a quote about cheese";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
