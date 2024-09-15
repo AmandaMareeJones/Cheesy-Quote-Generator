d0fe9ae8a9bba5ff2c3c1ef50cb70d41b9c27755
Amanda Maree Jones <amandamareejones@gmail.com>
2024-09-15 16:29:41 +1000

    JS - added typewriter and basic script

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let apiKey = "09te212a1c952dfb01916b44eoad6c23";
-let context =
-  "you are an intelligent AI Assistant, who provides funny quotes about cheese";
-let prompt = "tell me a quote about cheese";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+function generateQuote(event) {
+  event.preventDefault();
+  new Typewriter("#quote", {
+    strings: "Your quote will appear here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-form");
+quoteFormElement.addEventListener("submit", generateQuote);
