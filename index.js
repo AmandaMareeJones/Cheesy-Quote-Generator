07f69232f5c8d36ef539461c775812ceba35f511
Amanda Maree Jones <amandamareejones@gmail.com>
2024-09-15 17:21:52 +1000

    JS - connected AI via API (Axios)

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function generateQuote(event) {
-  event.preventDefault();
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings: "Your quote will appear here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let userCheese = document.querySelector("#user-submit");
+  let apiKey = "09te212a1c952dfb01916b44eoad6c23";
+  let context =
+    "you are an intelligent AI Assistant, who provides funny quotes about the type of cheese type submitted by the user. You need to read the value the user submits in the form.You need to identify the cheese type the user has provided before they click the submit button. The user always provides their choice of cheese. If you do not read a cheese type, provide a cheesy quote anyway";
+  let prompt = `User Submit: Tell me a quote about ${userCheese.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating quote");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-form");
